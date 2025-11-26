4994ac393c5c82c2fd96e4039fb996975d402430
mtarugay <2mikhailarugay1919@gmail.com>
2025-11-27 07:49:07 +1300

    Created the views, and modified home.html to show buttons.

--- spa/urls.py
+++ spa/urls.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 urlpatterns = [
+    path("home/", views.view_articles, name="home"),
+    path("add/", views.add_article, name="add"),
+    path("modify/", views.modify_article, name="modify"),
+    path("remove/", views.remove_article, name="remove")
 ]
+# still need multiple urls for add/modify/remove requirements. this is still single-page
--- spa/views.py
+++ spa/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
-from .models import Category, Article
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Role, User, Category, Article
+from .forms import ArticleForm
+from django.contrib import messages
 
+# Create your views here.
+
+def view_articles(request):
+    category = request.GET.get("category")
+    query = request.GET.get("q")
+    
+    articles = Article.objects.all()
+    
+    if category:
+        articles = articles.filter(category_name__id=category)
+    if query:
+        articles = articles.filter(article_name__icontains=query)
+        
+    return render(request, "home.html", {"articles": articles})
+
+
+@login_required
+def add_article(request):
+    if not request.user.is_tutor():
+        return redirect("home")
+    
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+        
+    else:
+        form = ArticleForm()
+    
+    articles = Article.objects.all()
+    
+    return render(request, "home.html", {"articles": articles, "form": form, "mode": "add"})
+
+
+@login_required
+def modify_article(request):
+    if not request.user.is_tutor():
+        return redirect("home")
+
+    article_id = request.GET.get("edit")
+    if not article_id:
+        return redirect("home")
+
+    article = get_object_or_404(Article, id=article_id)
+
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = ArticleForm(instance=article)
+
+    articles = Article.objects.all()
+
+    return render(request, "home.html", {"articles": articles, "form": form, "article_to_edit": article, "mode": "edit",})
+
+
+@login_required
+def remove_article(request):
+    if not request.user.is_admin():
+        return redirect("home")
+
+    article_id = request.GET.get("delete")
+    if not article_id:
+        return redirect("home")
+
+    article = get_object_or_404(Article, id=article_id)
+
+    if request.method == "POST":
+        article.delete()
+        messages.success(request, "Article removed.")
+        return redirect("home")
+
+    articles = Article.objects.all()
+
+    return render(request, "home.html", {"articles": articles, "article_to_delete": article, "mode": "delete",
+    })
